68be9f310c300b365150ff6eb50e2504b7a9adff
Stanislas Polu <stan@teleportd.com>
2012-04-11 13:44:15 +0200

    updated example

--- nodejs/test/simple.js
+++ nodejs/test/simple.js
@@ -1,6 +1,8 @@
 var util = require('util');
 
-var tl = require('../lib/teleportd.js').teleportd({ apikey: '02bc638df5c4fce00d086f57c230648a9e193d296bbd7a33a9efa53acd46df11cf82ccc06c38a6e390407e44bc05b27e657ad81f860136a3f62787b4638411de' });
+var user_key = 'XXX____YOUR_USER_KEY____XXX';
+
+var tl = require('../lib/teleportd.js').teleportd({ user_key: user_key });
 
 
 /*
@@ -17,15 +19,16 @@ tl.get('12-01-31-fb2fcc847a96681ef9cd61c011a6f93e3d92ef7f',
        });
 */
 
-var sid = tl.stream({ string: 'src:instagram' }, 
+var sid = tl.stream({ track: ['paris'] }, 
 		    function(pic) {
 		      if(typeof pic == 'undefined')
 			util.debug('STREAM: END!');
 		      else
 			util.debug('STREAM: ' + util.inspect(pic));
 		    });
-
+/*
 setTimeout(function() {
   console.log('TOP!');
     tl.stop(sid);
 }, 1000);
+*/
